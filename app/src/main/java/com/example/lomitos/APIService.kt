a95eac77add009097b0c23423acfc1be04e766dc
julio <jcah549@gmail.com>
2021-04-27 14:09:21 -0500

    proyecto de lomitos

---
+++ app/src/main/java/com/example/lomitos/APIService.kt
@@ -0,0 +1,19 @@
+package com.example.lomitos
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+
+
+    @GET
+   suspend fun getDogsByBreeds(@Url url:String):Response<DogsResponse>
+
+
+
+
+
+
+
+}
